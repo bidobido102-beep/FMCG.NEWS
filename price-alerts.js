3a1e9c78ce7a7e4a0befbbb5904fcb93507d0d8c
bidobido102-beep <bidobido102@gmail.com>
2026-04-22 10:36:07 +0200

    Create price-alerts.js

---
+++ price-alerts.js
@@ -0,0 +1,15 @@
+const PRICE_ALERTS = [
+  {
+    title: "زيادة أسعار بعض منتجات السجائر",
+    category: "سجائر",
+    source: "اليوم السابع",
+    date: "21 إبريل 2026"
+  },
+  {
+    title: "إيديتا تراجع أسعار المخبوزات",
+    category: "FMCG",
+    source: "Enterprise",
+    date: "20 إبريل 2026"
+  }
+];
+``
